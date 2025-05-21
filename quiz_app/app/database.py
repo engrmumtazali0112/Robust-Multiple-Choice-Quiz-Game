f45dcd7773e70efb17a22a018ba8efb9a1e71f37
Mumtaz Ali <engrmumtazali01@gmail.com>
2025-05-21 15:32:36 +0500

    Update database.py

--- quiz_app/app/database.py
+++ quiz_app/app/database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Update this connection string with your PostgreSQL credentials
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/quiz_db"
+DATABASE_URL = "postgresql://postgres:ganikhan12%40%23@localhost:5432/quiz_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
